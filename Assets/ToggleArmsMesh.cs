76a990a287b06099b645d588e5ee0a684451c363
HienaDev <ant.rodrigues1999@gmail.com>
2024-09-30 10:23:54 +0100

    Add brocolli

--- Assets/ToggleArmsMesh.cs
+++ Assets/ToggleArmsMesh.cs
@@ -3,8 +3,22 @@ using UnityEngine;
 public class ToggleArmsMesh : MonoBehaviour
 {
     [SerializeField] private GameObject arms;
+    [SerializeField] private GameObject phoneOnHand;
+    [SerializeField] private GameObject phoneOnStand;
 
-    public void ActivateArms() => arms.SetActive(true);
+    public void ActivateArms()
+    {
+        arms.SetActive(true);
+        phoneOnHand.SetActive(true);
+        phoneOnStand.SetActive(false);
+    }
 
-    public void DeactivateArms() => arms.SetActive(false);
+    public void DeactivateArms()
+    {
+        phoneOnHand.SetActive(false);
+        arms.SetActive(false);
+        Debug.Log(phoneOnHand.name);
+        
+        phoneOnStand.SetActive(true);
+    }
 }
