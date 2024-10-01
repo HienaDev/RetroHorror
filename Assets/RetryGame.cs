64fdf45ab2ec151ed536d24e8873fe719918acd0
HienaDev <ant.rodrigues1999@gmail.com>
2024-10-01 17:37:20 +0100

    idk

--- Assets/RetryGame.cs
+++ Assets/RetryGame.cs
@@ -1,22 +1,21 @@
-
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class FlashScreen : MonoBehaviour
+public class RetryGame : MonoBehaviour
 {
-
+    [SerializeField] private KeyCode retryKey = KeyCode.R;
     [SerializeField] private string sceneName;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(retryKey)) { LoadSelectedScene(); }
     }
 
     public void LoadSelectedScene() => SceneManager.LoadScene(sceneName);
